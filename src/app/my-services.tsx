2f0ffe07cf20dfad33d843dae02ab56525e2c62b
Thiago José Farias <52984475+sycrot@users.noreply.github.com>
2024-02-05 16:28:27 -0300

    fix: my services links

--- src/app/my-services.tsx
+++ src/app/my-services.tsx
@@ -57,7 +57,7 @@ export default function MyServices() {
               <div className="position-relative">
                 <h6 className="font-s28 color-marineblue fw-bold">Desenvolvimento / Programação</h6>
                 <p>Como desenvolvedor, trabalho com a criação de layouts responsivos que se adaptam a diferentes dispositivos, proporcionando uma experiência de usuário consistente e intuitiva. Além disso, desenvolvo Lading Pages eficientes, que visam converter visitantes em leads ou clientes, por meio de um design atrativo e conteúdo persuasivo. Trabalho no desenvolvimento de aplicações e sites web que atendem às necessidades específicas dos usuários, oferecendo funcionalidades e interatividade de acordo com os objetivos do cliente. Meu foco é sempre garantir a usabilidade, acessibilidade e eficiência em todos os projetos de design e desenvolvimento web.</p>
-                <ViewWorksButton link="/portolio/desenvolvimento" />
+                <ViewWorksButton link="/portfolio/devlopment" />
               </div>
             </motion.div>
           </div>
@@ -71,7 +71,7 @@ export default function MyServices() {
               <div className="position-relative">
                 <h6 className="font-s28 fw-bold">Design e prototipagem</h6>
                 <p>Como profissional de design, minha expertise abrange uma variedade de áreas, incluindo social media design, design gráfico, motion design, edição de vídeo e web design. Meu trabalho envolve a criação de conteúdo visual atraente e impactante para plataformas de mídia social, com foco em engajamento e alcance do público-alvo.<br /><br />Trabalho na concepção e desenvolvimento de interfaces digitais, garantindo a usabilidade, acessibilidade e estética de sites e aplicativos. Meu objetivo é sempre oferecer soluções visuais criativas e eficazes, alinhadas às necessidades e objetivos de cada projeto.</p>
-                <ViewWorksButton link="/portolio/design" />
+                <ViewWorksButton link="/portfolio/design" />
               </div>
             </motion.div>
           </div>
